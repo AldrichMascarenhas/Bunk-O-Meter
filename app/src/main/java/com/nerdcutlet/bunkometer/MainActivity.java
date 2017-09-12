35c03327bc2bd0b9b9a07889a647ff05aacf9ab9
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2017-09-13 01:12:27 +0530

    #7 Add Buttons/Text for other days

--- app/src/main/java/com/nerdcutlet/bunkometer/MainActivity.java
+++ app/src/main/java/com/nerdcutlet/bunkometer/MainActivity.java
@@ -12,8 +12,8 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     //Widgets
-    Button button_mon;
-    TextView textView_mon;
+    Button button_mon, button_tue, button_wed, button_thu, button_fri;
+    TextView textView_mon, textView_tue, textView_wed, textView_thu, textView_fri;
 
     //Class Variables
     int num_mon;
@@ -31,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
         //Views
         button_mon = (Button) findViewById(R.id.mon);
         textView_mon = (TextView) findViewById(R.id.mon_text);
+
+        button_tue = (Button) findViewById(R.id.tue);
+        textView_tue = (TextView) findViewById(R.id.tue_text);
+
+        button_wed = (Button) findViewById(R.id.wed);
+        textView_wed = (TextView) findViewById(R.id.wed_text);
+
+        button_thu = (Button) findViewById(R.id.thu);
+        textView_thu = (TextView) findViewById(R.id.thu_text);
+
+        button_fri = (Button) findViewById(R.id.fri);
+        textView_fri = (TextView) findViewById(R.id.fri_text);
+
 
         //Actions
         button_mon.setOnClickListener(new View.OnClickListener() {
