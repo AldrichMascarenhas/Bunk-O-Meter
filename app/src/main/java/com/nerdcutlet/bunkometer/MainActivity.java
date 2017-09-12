a65b69c36fb66c18c934b2273ec166c33b5550f6
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2017-09-12 22:46:27 +0530

    #1 Add Monday Button and Text

--- app/src/main/java/com/nerdcutlet/bunkometer/MainActivity.java
+++ app/src/main/java/com/nerdcutlet/bunkometer/MainActivity.java
@@ -2,12 +2,21 @@ package com.nerdcutlet.bunkometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_mon;
+    TextView textView_mon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_mon = (Button) findViewById(R.id.mon);
+        textView_mon = (TextView)findViewById(R.id.mon_text);
+
     }
 }
